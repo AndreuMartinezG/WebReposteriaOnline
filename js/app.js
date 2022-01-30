7fbf1f71bffddbcd50de4c42321df12cf4520215
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-01-30 17:11:11 +0100

    Modificado Presentacion visual del titulo en las paginas

--- js/app.js
+++ js/app.js
@@ -1,14 +1,33 @@
+
+// Registramos todas las cards(productos) de las paginas
+const items = document.getElementById('items');
+console.log(items)
+
+// Esperamos que este el DOM completamente cargado
 document.addEventListener('DOMContentLoaded', (event) => {
     fetchData();
-    console.log("DOM Loaded")
-})
+    console.log("DOM Loaded");
+});
 
+
+// Detectamos el evento de click de los botones de Añadir al Carrito
+items.addEventListener('click', e => {
+    addCarrito(e)
+});
+
+
+// Captamos la informacion de db.json
 const fetchData = async () => {
     try {
         const res = await fetch('/js/db.json');
         const data = await res.json();
-        console.log(data)
+        console.log(data);
     } catch (error) {
         console.log(error);
     }
 };
+
+
+const addCarrito = e => {
+    console.log(e.target)
+}
