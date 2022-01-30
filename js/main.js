fa27d4f5624a9200301e053527da840aa4ad09ab
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-01-30 13:57:32 +0100

    Pruebas con el main.js y db.json

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const agua = () => {
+    aguaObj.hasComprado++
+}
+
+let aguaObj =     {
+    "precio": 1.5,
+    "id": 1,
+    "title": "Agua",
+    "hasComprado": 0 
+};
+
+const dibujar = () => {
+
+}
+
